b0e08edfe37e85b6a95027dd0027a5373cf765ac
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-10 22:19:57 -0800

    added more documentation

--- isaiahgamboa.c
+++ isaiahgamboa.c
@@ -1,3 +1,16 @@
+/*The program compares the disk scheduling algorithms: First-in-first-out (FIFO),
+Shortest-seek-time-first (STTF), Scan, and C-Scan in terms of traversing a set of tracks
+input by the user, and calculates the total distance of the tracks traversed.
+ A menu controls the operations, and each choice calls the appropriate procedure, where
+the choices are:
+1) Enter parameter
+2) Calculate distance to traverse tracks using FIFO
+3) Calculate distance to traverse tracks using SSTF
+4) Calculate distance to traverse tracks using Scan
+5) Calculate distance to traverse tracks using C-Scan
+6) Quit program and free memory
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
