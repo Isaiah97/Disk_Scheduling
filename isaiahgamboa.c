a3405b6fb2f952901ec488c7b6b887f0168e1bc7
Isaiah <isaiah.gamboa.383@my.csun.edu>
2025-12-10 18:29:49 -0800

    library setup

--- isaiahgamboa.c
+++ isaiahgamboa.c
@@ -2,4 +2,11 @@
 #include <stdlib.h>
 #include <limits.h>
 
+//need global variables
+
+int disk_size = 0;			// Number of tracks on the disk (tracks are 0 .. disk_size-1)
+int num_requests = 0;		// How many track requests the user gave
+int start_track = 0;		// Starting position of the disk head
+int *requests = NULL;		// Dynamic array of track requests
+int params_set = 0;			// Flag: have we entered parameters yet?
 
